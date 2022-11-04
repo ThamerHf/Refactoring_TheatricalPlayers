17f6f6423102cf4088df74852d912c1f0ed4cd77
thamer <thamerhafsouni1@gmail.com>
2022-11-04 14:28:31 +0100

    use of the function playAmount to calcul thisAmount without Switch

--- TheatricalPlayers/src/main/java/InvoiceCalcul.java
+++ TheatricalPlayers/src/main/java/InvoiceCalcul.java
@@ -13,26 +13,21 @@ public class InvoiceCalcul{
         NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
         for (Performance perf : invoice.performances) {
-        Play play = plays.get(perf.playID);
-        float thisAmount = 0;
+            Play play = plays.get(perf.playID);
 
-        switch (play.type) {
-            case "tragedy":
-            thisAmount = play.playAmount(perf);
-            break;
-            case "comedy":
-            thisAmount = play.playAmount(perf);
-            break;
-            default:
-            throw new Error("unknown type: ${play.type}");
-        }
+            // calcul the amount for this play type
+            float thisAmount = play.playAmount(perf);
 
-        // add volume credits
-        volumeCredits += play.playCredit(perf);
+            if(thisAmount == -1.0){
+                throw new Error("unknown type: ${play.type}");
+            }
 
-        // print line for this order
-        result.append(String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount), perf.audience));
-        totalAmount += thisAmount;
+            // add volume credits
+            volumeCredits += play.playCredit(perf);
+
+            // print line for this order
+            result.append(String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount), perf.audience));
+            totalAmount += thisAmount;
         }
         result.append(String.format("Amount owed is %s\nYou earned %s credits\n", frmt.format(totalAmount), volumeCredits));
         return result;
