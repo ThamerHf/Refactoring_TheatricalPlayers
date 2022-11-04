68a1e8d8555d8a38b05b1e7755aeab9b328cf3c4
thamer <thamerhafsouni1@gmail.com>
2022-11-04 14:02:13 +0100

    Create a function InvoiceCalcul to calculate the invoice and generate of a text format and an html format of this facture

--- TheatricalPlayers/src/main/java/StatementPrinter.java
+++ TheatricalPlayers/src/main/java/StatementPrinter.java
@@ -12,37 +12,7 @@ public class StatementPrinter {
 /*Output: a String representing the invoice                 		*/
 /********************************************************************/
   public StringBuilder print(Invoice invoice, Map<String, Play> plays) {
-    float totalAmount = 0;
-    int volumeCredits = 0;
-    StringBuilder result = new StringBuilder(String.format("Statement for %s\n", invoice.customer));
-
-    NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
-
-    for (Performance perf : invoice.performances) {
-      Play play = plays.get(perf.playID);
-      float thisAmount = 0;
-
-      switch (play.type) {
-        case "tragedy":
-          thisAmount = play.playAmount(perf);
-          break;
-        case "comedy":
-          thisAmount = play.playAmount(perf);
-          break;
-        default:
-          throw new Error("unknown type: ${play.type}");
-      }
-
-      // add volume credits
-      volumeCredits += Math.max(perf.audience - 30, 0);
-      // add extra credit for every ten comedy attendees
-      if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
-
-      // print line for this order
-      result.append(String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount), perf.audience));
-      totalAmount += thisAmount;
-    }
-    result.append(String.format("Amount owed is %s\nYou earned %s credits\n", frmt.format(totalAmount), volumeCredits));
+    StringBuilder result = new InvoiceCalcul().toText(invoice, plays);
     return result;
   }
 
