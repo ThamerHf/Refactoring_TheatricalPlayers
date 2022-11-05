86e4cd69d56ee1f171bb334a4ae82cb119a69040
xxsalouhxx <lachehebsalah9@gmail.com>
2022-11-05 16:05:08 +0100

    Add main methode to test toHTML() method

--- TheatricalPlayers/src/main/java/InvoiceCalcul.java
+++ TheatricalPlayers/src/main/java/InvoiceCalcul.java
@@ -4,6 +4,9 @@ import java.lang.StringBuilder;
 
 public class InvoiceCalcul{
     public InvoiceCalcul(){}
+    /********************************************************************/
+    /* La methode toText() permet de génerer le résultat en format text */
+    /********************************************************************/
 
     public StringBuilder toText(Invoice invoice, Map<String, Play> plays){
         float totalAmount = 0;
@@ -13,28 +16,21 @@ public class InvoiceCalcul{
         NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
         for (Performance perf : invoice.performances) {
-        Play play = plays.get(perf.playID);
-        float thisAmount = 0;
+            Play play = plays.get(perf.playID);
 
-        switch (play.type) {
-            case "tragedy":
-            thisAmount = play.playAmount(perf);
-            break;
-            case "comedy":
-            thisAmount = play.playAmount(perf);
-            break;
-            default:
-            throw new Error("unknown type: ${play.type}");
-        }
+            // calcul the amount for this play type
+            float thisAmount = play.playAmount(perf);
 
-        // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
+            if(thisAmount == -1.0){
+                throw new Error("unknown type: ${play.type}");
+            }
 
-        // print line for this order
-        result.append(String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount), perf.audience));
-        totalAmount += thisAmount;
+            // add volume credits
+            volumeCredits += play.playCredit(perf);
+
+            // print line for this order
+            result.append(String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount), perf.audience));
+            totalAmount += thisAmount;
         }
         result.append(String.format("Amount owed is %s\nYou earned %s credits\n", frmt.format(totalAmount), volumeCredits));
         return result;
@@ -46,7 +42,7 @@ public class InvoiceCalcul{
         StringBuilder result = new StringBuilder(String.format("<html> \n<head> <title> Invoice </title></head> \n<body>\n <h1> Invoice </h1>\n"));
 
         result.append(String.format("<ul> <li> <b> Client : </b> %s </li> </ul> \n", invoice.customer));
-        result.append("<table> \n <tr> \n <td> Piece </td> \n <td> Seats sold </td> \n <td> Price </td> \n </tr> \n");
+        result.append("<table border='1'> \n <tr> \n <td> Piece </td> \n <td> Seats sold </td> \n <td> Price </td> \n </tr> \n");
         NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
         for (Performance perf : invoice.performances) {
@@ -74,8 +70,8 @@ public class InvoiceCalcul{
 
         totalAmount += thisAmount;
         }
-        result.append(String.format("<tr> \n <td> <b>Total Owned :</b> </td> \n <td> $%f </td> \n </tr> \n", frmt.format(totalAmount)));
-        result.append(String.format("<tr> \n <td> <b>Fidelity Points Earned :</b> </td> \n <td> %d </td> \n </tr> \n" ,volumeCredits));
+        result.append(String.format("<tr> \n <td colspan = '2'> <b>Total Owned :</b> </td> \n <td> $%s </td> \n </tr> \n", frmt.format(totalAmount)));
+        result.append(String.format("<tr> \n <td colspan = '2'> <b>Fidelity Points Earned :</b> </td> \n <td> %d </td> \n </tr> \n" ,volumeCredits));
         result.append("</table> \n </body> \n </html>");
         return result;
     }
